ee994fa841f7adc088197c4a2309ee5f0e90f98a
Margarita Rastriga <rasm@1Ci.com>
2023-08-05 23:36:09 +0300

    Выполнено: 1. Подключена СУБД, Созданы модели Product, Catalog, UserContact, Supplier. Созданы миграции. Создана админка. Создана кастомная команда для заполнения поставщиков.

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
-
-# Create your views here.
+from catalog.models import UserContact, Product
 
 
 def index(request):
-    return render(request, "catalog/index.html")
+    """Отображаем первую страницу, выводим первые три продукта в карточки, если база данных не пуста"""
+
+    try:
+        item_count = 0
+        products_list_3 = {}
+        for item in Product.objects.all():
+            item_count += 1
+            products_list_3[f"title{item_count}"] = item.title
+            products_list_3[f"price{item_count}"] = item.price
+            products_list_3[f"description{item_count}"] = item.description
+            if item_count > 3:
+                break
+        return render(request, "catalog/index.html", context=products_list_3)
+    except Exception:
+        return render(request, "catalog/index.html")
 
 
 def contacts_post(request):
+    """Передаем в базу данных то что прислал пользователь"""
+
     if request.method == 'POST':
-        # в переменной request хранится информация о методе, который отправлял пользователь
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        # а также передается информация, которую заполнил пользователь
-        with open("user_returns.txt", mode="a") as file:
-            file.write(f"{name}, {phone}, {message}")
+        UserContact.objects.create(name=request.POST.get('name'),
+                                   phone=request.POST.get('phone'),
+                                   message=request.POST.get('message'))
 
     return render(request, 'catalog/contacts.html')
