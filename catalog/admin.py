fd330a6f364c06a37309091c68a4e08c064b9867
Margarita Rastriga <rasm@1Ci.com>
2023-08-21 14:28:10 +0300

    Выполнено: Добавлена страничка с блогом и реализован CRUD

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category, UserContact, Supplier
+from blog.models import Blog
+from catalog.models import Product, Category, Usercontact, Supplier
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'price', 'category', 'description',)
+    list_display = ('id', 'title', 'price', 'category', 'description', 'slug', 'picture')
     list_filter = ('category', 'title')
     search_fields = ('title', 'description',)
 
@@ -16,7 +17,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('title',)
 
 
-@admin.register(UserContact)
+@admin.register(Usercontact)
 class UserContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone', 'message')
 
@@ -24,3 +25,8 @@ class UserContactAdmin(admin.ModelAdmin):
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'content', 'preview', 'publication_date', 'published', 'views_number',)
