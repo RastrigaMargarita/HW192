c857892adb3402884ef6e5d179ec8faaaa604879
Margarita Rastriga <rasm@1Ci.com>
2023-08-23 23:39:30 +0300

    Добавлены файлы приложения.

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,9 @@ class Blog(models.Model):
     slug = models.CharField(max_length=150, verbose_name="slug", db_index=True, unique=True)
     content = models.TextField(verbose_name="Содержимое")
     preview = models.ImageField(verbose_name="изображение (превью)", null=True)
-    publication_date = models.DateTimeField(verbose_name="Дата публикации")
+    publication_date = models.DateTimeField(verbose_name="Дата публикации", auto_now_add=True)
     published = models.BooleanField()
-    views_number = models.IntegerField(null=True)
+    views_number = models.IntegerField(default=0)
     comments = models.TextField(null=True)
 
 
