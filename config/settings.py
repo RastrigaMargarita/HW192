6643e5ec1a8179b5f1b8f72d7a0bb2fcf63710aa
Margarita Rastriga <rasm@1Ci.com>
2023-11-28 23:29:29 +0300

    Проблемы с .env

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dot_env = os.path.join(BASE_DIR, '.envir')
+dot_env = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path=dot_env)
 
 # Quick-start development settings - unsuitable for production
